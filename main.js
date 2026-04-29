434cabedc11cfd7f3bc6764e2148533a009ba639
tropical-express <gnome2009123@gmail.com>
2026-04-29 08:45:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 750,
+    webPreferences: {
+       contextIsolation: true,
+      webviewTag: true
+    },
+  });
+      
+     
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
